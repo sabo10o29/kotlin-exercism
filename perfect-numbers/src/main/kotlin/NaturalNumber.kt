7382ffcdfb4a8d5f2124224585e1c3434a922519
sabo10o29 <alex29sabo10@gmail.com>
2019-09-22 16:34:38 +0900

    work record: 2019-09-22

--- perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -1,8 +1,21 @@
+import java.lang.RuntimeException
 
 enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
 fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
+    if(naturalNumber <= 0) throw RuntimeException()
+
+    var sum = 0
+    for(i in 1.. (naturalNumber/2)){
+        if(naturalNumber%i == 0) sum += i
+    }
+
+    return when{
+        sum == naturalNumber -> Classification.PERFECT
+        sum > naturalNumber  -> Classification.ABUNDANT
+        sum < naturalNumber  -> Classification.DEFICIENT
+        else                 -> throw RuntimeException()
+    }
 }
